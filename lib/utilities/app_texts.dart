cc4a6044c8ca15b7be96e2e7236b3aface58db26
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-10-25 22:17:15 +0600

    app texts class added

---
+++ lib/utilities/app_texts.dart
@@ -0,0 +1,7 @@
+class AppTexts {
+  static const String appTitle = "Bike Price Check";
+  static const String fontBold = "bold";
+  static const String fontRegular = "regular";
+  static const String welcome = "Welcome";
+  static const String bikePricePredictionApp = "Bike Price Prediction App";
+}
