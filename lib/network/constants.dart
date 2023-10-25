035f0af9f6c0e3755eb2f92f7f75a98538c8dead
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-10-26 02:14:22 +0600

    constant values added

---
+++ lib/network/constants.dart
@@ -0,0 +1,7 @@
+class Constant{
+  static const post = "POST";
+  static const brand = "Brand";
+  static const manufacturedYear = "Manufactured_year";
+  static const travel = "Speed(km)";
+  static const engine = "Engine(cc)";
+}
